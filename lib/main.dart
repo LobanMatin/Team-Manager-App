1fb057a56b8cca2233e488933e34479788adfc23
Loban Matin <loban.matin@gmail.com>
2024-07-03 19:18:12 +0600

    authentication initialization

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,16 @@ import 'package:team_manager_application/pages/content_page.dart';
 import 'package:team_manager_application/pages/login_page.dart';
 import 'package:team_manager_application/pages/singup_page.dart';
 import 'package:team_manager_application/pages/start_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   runApp(const TeamApp());
 }
 
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class AuthService {
+  Future<void> signUp({required String email, required String password}) async {
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: email,
+            password: password
+            );
+
+    } on FirebaseAuthException catch(e) {
+      String message = '';
+      if (e.code == 'weak-password') {
+        message = 'The password is too weak';
+      } else if (e.code == 'email-already-in-use') {
+        message = 'An account already exists with that email';
+      }
+      
+      Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 14.0,
+      );
+    }
+  }
+}
