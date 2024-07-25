9d86bb0226ff44edcf9082c8d97bd2f77ccab279
Loban Matin <loban.matin@gmail.com>
2024-07-25 22:26:07 +1000

    added all database info to app

--- lib/pages/content_page.dart
+++ lib/pages/content_page.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:team_manager_application/models/training_model.dart';
 import 'package:team_manager_application/services/auth_service.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class ContentPage extends StatefulWidget {
   const ContentPage({super.key});
@@ -11,16 +14,22 @@ class ContentPage extends StatefulWidget {
 }
 
 class _ContentPageState extends State<ContentPage> {
-
   DatabaseReference dbRef = FirebaseDatabase.instance.ref();
 
   List<TrainingInstance> trainingList = [];
+  List<String> terminology = [];
+  Map<dynamic, dynamic> poomsae = {};
+  String randomTip = "";
+  final beltLevel = 'black'; // make this user dependent
+
+  late Map<dynamic, YoutubePlayerController> _controllerList = {};
 
   @override
   void initState() {
     super.initState();
 
     retrieveTrainingData();
+    retrieveResourceData();
   }
 
   @override
@@ -63,12 +72,24 @@ class _ContentPageState extends State<ContentPage> {
               Expanded(
                 flex: 2,
                 child: Container(
-                  color: Colors.red,
+                  color: Colors.transparent,
                   child: SingleChildScrollView(
-                    child: Column(children: [
-                      for (int i = 0; i < trainingList.length; i++)
-                        trainingWidget(trainingList[i])
-                    ],)
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                          child: Text(
+                            "This Week's Training Sessions:",
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ),
+                        for (int i = 0; i < trainingList.length; i++)
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                            child: trainingWidget(trainingList[i]),
+                          )
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -79,15 +100,50 @@ class _ContentPageState extends State<ContentPage> {
                     Expanded(
                       flex: 1,
                       child: Container(
-                        color: Colors.blue,
-                        child: const Placeholder(),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.yellow,
-                        child: const Placeholder(),
+                        color: Colors.transparent,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Text(
+                                "Tips & Resources",
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              Text(
+                                "Korean Terminology",
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              for (int i = 0; i < terminology.length; i++)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                                  child: Text(terminology[i].toString()),
+                                ),
+                              Text(
+                                "Poomsae Resources",
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              for (var key in poomsae.keys)
+                                Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                                    child: Column(
+                                      children: [
+                                        Text(key.toString()),
+                                        YoutubePlayer(
+                                          controller: _controllerList[key.toString()]!,
+                                        ),
+                                      ],
+                                    )),
+                              Text(
+                                "Helpful Tip",
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              Text(
+                                randomTip,
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ],
@@ -99,32 +155,65 @@ class _ContentPageState extends State<ContentPage> {
       ),
     );
   }
-  
+
   Future<void> retrieveTrainingData() async {
-    final data2 = await dbRef.child("Trainings").get() as Map;
-    for (var session in data2.keys) {
-      final datapoint = data2[data2[session]];
-      TrainingInstance training = TrainingInstance(key: session, trainingData: datapoint);
+    final data = (await dbRef.child("trainings").get()).value as Map;
+    for (var session in data.keys) {
+      final datapoint = TrainingData.fromJson((data[session]));
+      TrainingInstance training =
+          TrainingInstance(key: session, trainingData: datapoint);
       trainingList.add(training);
       setState(() {});
     }
   }
-  
+
   Widget trainingWidget(TrainingInstance trainingList) {
     return Container(
       width: MediaQuery.of(context).size.width,
       padding: const EdgeInsets.all(5),
       margin: const EdgeInsets.only(top: 5, left: 10, right: 10),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.black)),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Column(children: [
-            Text(trainingList.trainingData!.datetime!),
-            Text(trainingList.trainingData!.location!),
-            Text(trainingList.trainingData!.description!),
-          ],)
-      ],),
+          Column(
+            children: [
+              Text(trainingList.trainingData!.datetime!),
+              Text(trainingList.trainingData!.location!),
+              Text(trainingList.trainingData!.description!),
+            ],
+          )
+        ],
+      ),
     );
+  }
+
+  Future<void> retrieveResourceData() async {
+    final resourceData =
+        (await dbRef.child("resources/$beltLevel").get()).value as Map;
+    final tipsData = (await dbRef.child("tips").get()).value as Map;
+
+    terminology = [
+      for (var tech in resourceData["korean"].keys) resourceData["korean"][tech]
+    ];
+
+    poomsae =
+        (await dbRef.child("resources/$beltLevel/poomsae").get()).value as Map;
+
+    for (var title in poomsae.keys) {
+      _controllerList[title.toString()] = YoutubePlayerController(
+        initialVideoId: poomsae[title],
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ),
+      );
+    }
+
+    var randInt = Random().nextInt(10);
+    randomTip = tipsData['t$randInt'].toString();
+
+    setState(() {});
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import firebase_auth
 import firebase_core
 import firebase_database
+import flutter_inappwebview_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
 }
